caa1e4131c1479ae7892b5a84e2e82dab258ab67
SmarTechPCs <43375167+SmarTechPCs@users.noreply.github.com>
2022-05-07 22:47:48 +0300

    Updated mini doc

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # python fun practicing
-# personal implementation for python split function for string (whitespaces delimiters only)
+# personal implementation for python split function of string (whitespaces delimiters only)
 # returns a list of the substrings of a given string
 # as separated by whitespaces (new line, space, etc..)
 #
